80bdd1bb20e09c61cc031805a78c311f6741dbf9
kiki3177 <150294655+kiki3177@users.noreply.github.com>
2024-07-22 15:12:32 -0700

    Gravity model update
    Add argparse and test run command line

--- models/gravity.py
+++ models/gravity.py
@@ -3,8 +3,26 @@ import geopandas as gpd
 import skmob
 import numpy as np
 from skmob.models.gravity import Gravity
+import argparse
 
 pd.set_option('display.max_columns', None)
+
+
+
+# Settings
+parser = argparse.ArgumentParser(description="Run gravity model.")
+parser.add_argument('tessellation_file_train_location', type=str,
+                    help="Path to the training tessellation .geojson file.")
+parser.add_argument('tessellation_file_test_location', type=str,
+                    help="Path to the test tessellation .geojson file.")
+parser.add_argument('flow_data_file_train_location', type=str,
+                    help="Path to the training flow data .csv file.")
+parser.add_argument('flow_data_file_test_location', type=str,
+                    help="Path to the test flow data .csv file.")
+parser.add_argument('gravity_type', type=str, default='singly_constrained', choices=['singly_constrained', 'globally_constrained'],
+                    help="Type of gravity model, 'singly_constrained' or 'globally'. Default is 'singly_constrained'.")
+parser.add_argument('out_format', type=str, default='flows', choices=['flows', 'probabilities'],
+                    help="Type of output, 'flows' or 'probabilities'. Default is 'flows'.")
 
 
 def grav_Model(tessellation_file_train_location, tessellation_file_test_location,
@@ -42,7 +60,13 @@ def grav_Model(tessellation_file_train_location, tessellation_file_test_location
     tessellation_test_merge = tessellation_test.merge(outflows_test, left_on='GEOID', right_on='origin').rename(columns={'flow': 'total_outflows'})
 
     # Create an instance of gravity model for fitting
-    gravity_singly_fitted = Gravity(gravity_type=gravity_type)
+    if gravity_type == 'singly_constrained':
+        gravity_type_use = 'singly constrained'
+    elif gravity_type == 'globally_constrained':
+        gravity_type_use = 'globally constrained'
+    else:
+        print("Invalid gravity type.")
+    gravity_singly_fitted = Gravity(gravity_type=gravity_type_use)
 
     # Fit the gravity model to the flow data using 'total_population'
     gravity_singly_fitted.fit(flow_data_train, relevance_column='total_population')
@@ -58,33 +82,28 @@ def grav_Model(tessellation_file_train_location, tessellation_file_test_location
     # Rename the synthetic column and save to .csv file
     if out_format == 'flows':
         synth_fdf_fitted.rename(columns={'flow': 'synthetic_flows'}, inplace=True)
-        synth_fdf_fitted.to_csv('../outputs/gravity_synthetic_flows.csv', index=False)
+        synth_fdf_fitted.to_csv('../crowdflow-fairness/outputs/gravity_synthetic_flows.csv', index=False)
     elif out_format == 'probabilities':
         synth_fdf_fitted.rename(columns={'flow': 'synthetic_probabilities'}, inplace=True)
-        synth_fdf_fitted.to_csv('../outputs/gravity_synthetic_probabilities.csv', index=False)
+        synth_fdf_fitted.to_csv('../crowdflow-fairness/outputs/gravity_synthetic_probabilities.csv', index=False)
     else:
         print("Invalid outputs format.")
 
+
+def main():
+    args = parser.parse_args()
+    grav_Model(args.tessellation_file_train_location, args.tessellation_file_test_location,
+               args.flow_data_file_train_location, args.flow_data_file_test_location,
+               args.gravity_type, args.out_format)
+
+
+
 if __name__ == "__main__":
-    tessellation_file_train_location = input("Enter the location of the training tessellation .geojson file: ")
-    tessellation_file_test_location = input("Enter the location of the test tessellation .geojson file: ")
-    flow_data_file_train_location = input("Enter the location of the training flow data .csv file: ")
-    flow_data_file_test_location = input("Enter the location of the test flow data .csv file: ")
-    gravity_type = input("Enter 'singly constrained' or 'globally constrained': ")
-    out_format = input("Enter 'flows' or 'probabilities': ")
-
-    grav_Model(tessellation_file_train_location, tessellation_file_test_location,
-               flow_data_file_train_location, flow_data_file_test_location,
-               gravity_type, out_format)
+    main()
 
 
 
-
-'''
-Test run:
-change to your local path
-tessellation_file_train_location = '/Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/tessellation_use_this.geojson'
-tessellation_file_test_location = '/Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/tessellation_use_this.geojson'
-flow_data_file_train_location = '/Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/flows.csv'
-flow_data_file_test_location = '/Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/flows.csv'
-'''
+"""
+Test run command line:
+python models/gravity.py /Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/tessellation_use_this.geojson /Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/tessellation_use_this.geojson /Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/flows.csv /Users/apple/Documents/GitHub/DSSG/crowdflow-fairness/data/flows.csv singly_constrained flows
+"""
